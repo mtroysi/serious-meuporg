84b7e3b4be152f6bad89922651326256d1e64db2
adrien3 <adrien.caselles@gmail.com>
2017-05-26 14:43:44 +0200

    Affichage de la poubelle cachée

--- src/main/resources/static/js/inventory/InventoryController.js
+++ src/main/resources/static/js/inventory/InventoryController.js
@@ -12,7 +12,7 @@
             ctrl.inventory = [];
 
             ctrl.init = function() {
-                InventoryService.getInventory(Number(AuthenticationService.getUserId())).then(function (data) {
+                InventoryService.getInventory(Number(AuthenticationService.getUserId())).then(function(data) {
                     ctrl.inventory = data;
                     ctrl.filteredInventory = data;
                 });
@@ -28,31 +28,31 @@
                 $(element).slideToggle(500);
             };
 
-            ctrl.filterItems = function () {
+            ctrl.filterItems = function() {
                 ctrl.filteredInventory = ctrl.inventory.filter(function(e) {
                     return e.type === ctrl.filter.type || ctrl.filter.type === 'TOUT';
                 });
             };
 
-            ctrl.removeFromInventory = function (item) {
+            ctrl.removeFromInventory = function(item) {
                 ctrl.itemToRemove = item;
 
-                ctrl.inventory = _.reject(ctrl.inventory, function (e) {
+                ctrl.inventory = _.reject(ctrl.inventory, function(e) {
                     return e.id === item.id;
                 });
 
-                CommonDialogService.confirmation('Êtes-vous sûr de vouloir jeter ' + item.name + ' ? Cette action est irréversible.', function () {
-                    InventoryService.updateInventory(ctrl.inventory).then(function (data) {
+                CommonDialogService.confirmation('Êtes-vous sûr de vouloir jeter ' + item.name + ' ? Cette action est irréversible.', function() {
+                    InventoryService.updateInventory(ctrl.inventory).then(function(data) {
                         ctrl.inventory = data;
                         ctrl.filteredInventory = data;
                     });
-                }, function () {
+                }, function() {
                     ctrl.inventory.push(ctrl.itemToRemove);
                 }, 'modalThrowItem', "Jeter un objet", "Valider", "Annuler");
+            };
 
-
-
-
+            ctrl.activeElement = function(idItem) {
+                alert(idItem);
             };
 
             ctrl.init();
