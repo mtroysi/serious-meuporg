fbda619bb25725f8c8bde3fa7ebde443be6f0eb7
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2017-05-27 16:34:01 +0200

    commentaires transformers

--- src/main/java/com/example/transformers/Transformers.java
+++ src/main/java/com/example/transformers/Transformers.java
@@ -1,18 +1,13 @@
 package com.example.transformers;
 
-import com.example.ConstanteGameMaster;
 import com.example.dto.BoardDTO;
 import com.example.dto.UserDTO;
 import com.example.model.Board;
-import com.example.model.BoardUser;
 import com.example.model.User;
-import com.example.repository.UserRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Morgane TROYSI on 11/05/17.
@@ -20,16 +15,33 @@ import java.util.List;
 @Service
 public class Transformers {
 
+    /**
+     * Convertit une entité en son DTO associé
+     * @param entity entité source à convertir
+     * @param dtoClass DTO de destination
+     * @return objet du type du DTO de destination
+     */
     public Object convertEntityToDto(Object entity, Class dtoClass) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(entity, dtoClass);
     }
 
+    /**
+     * Convertit un DTO en son entité associée
+     * @param dto dto source à convertir
+     * @param entityClass entité de destination
+     * @return objet du type de l'entité de destination
+     */
     public Object convertDtoToEntity(Object dto, Class entityClass) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(dto, entityClass);
     }
 
+    /**
+     * Méthode spécifique pour transformer un tableau en son DTO
+     * @param board tableau source
+     * @return BoardDTO tableau de destination
+     */
     public BoardDTO transformBoardToBoardDto(Board board) {
         BoardDTO boardDTO = (BoardDTO)convertEntityToDto(board, BoardDTO.class);
         boardDTO.setUsers(new ArrayList<>());
@@ -39,6 +51,11 @@ public class Transformers {
         return boardDTO;
     }
 
+    /**
+     * Méthode spécifique pour transformer un utilisateur en son DTO
+     * @param user utilisateur source
+     * @return UserDTO
+     */
     public UserDTO transformUserToUserDto(User user) {
         UserDTO userDTO = (UserDTO)convertEntityToDto(user, UserDTO.class);
         userDTO.setFullName(user.getFirstName() + ' ' + user.getLastName());
