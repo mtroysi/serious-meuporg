a4faaa057efef85c82956b78da394c247f1ab043
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2017-05-29 19:15:33 +0200

    comment notification

--- src/main/resources/static/js/notification/NotificationWS.js
+++ src/main/resources/static/js/notification/NotificationWS.js
@@ -10,14 +10,29 @@
     helloApp.service('NotificationWS', function($http, constant) {
         var svc = this;
 
+        /**
+         * Retourne les notifications d'un utilisateur
+         * @param idUser id de l'utilisateur
+         * @returns {HttpPromise}
+         */
         svc.getNotificationByUser = function(idUser) {
             return $http.get(constant.BASE_URI + '/notification/user/' + idUser);
         };
 
+        /**
+         * Crée une notification
+         * @param notification données de la notif à créer
+         * @returns {HttpPromise}
+         */
         svc.createNotification = function(notification) {
             return $http.post(constant.BASE_URI + '/notification/', notification);
         };
 
+        /**
+         * Marque toutes les notifications de l'utilisateur comme lues
+         * @param idUser id de l'utilisateur
+         * @returns {HttpPromise}
+         */
         svc.readAllNotification = function(idUser) {
             return $http.get(constant.BASE_URI + '/notification/user/' + idUser + '/read');
         };
