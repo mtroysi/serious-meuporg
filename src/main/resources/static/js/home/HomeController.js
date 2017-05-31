06294eb2d5de95bc2fe2a59d093cb949c8422cb2
adrien3 <adrien.caselles@gmail.com>
2017-05-31 13:55:32 +0200

    Page d'accueil : bouton pour remonter en haut de la page

--- src/main/resources/static/js/home/HomeController.js
+++ src/main/resources/static/js/home/HomeController.js
@@ -11,7 +11,19 @@
         var ctrl = this;
 
         ctrl.init = function() {
-            $('body').addClass('bg-white');
+            /* Gestion du bouton pour remonter tout en haut de la page */
+            $(window).scroll(function() {
+                if ($(this).scrollTop() >= 50) {
+                    $('#return-to-top').fadeIn(200);
+                } else {
+                    $('#return-to-top').fadeOut(200);
+                }
+            });
+            $('#return-to-top').click(function() {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 500);
+            });
         };
 
         ctrl.init();
